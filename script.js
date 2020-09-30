8dba0b308630956c517008e425bb7a6a693ae1f8
NaokiYonemaru <68312351+NaokiYonemaru@users.noreply.github.com>
2020-09-30 22:14:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+'use strict';
+
+{
+    const opens = document.getElementsByClassName('open');
+    const close = document.getElementById('close');
+    const modal = document.getElementById('modal');
+    const mask = document.getElementById('mask');
+
+    for (let open of opens) {
+        open.addEventListener('click', () => {
+            modal.classList.remove('hidden');
+            mask.classList.remove('hidden');
+        });
+    }    
+    close.addEventListener('click', () => {
+        modal.classList.add('hidden');
+        mask.classList.add('hidden');
+    });
+    mask.addEventListener('click', () => {
+        close.click();
+    });
+}
+
 $(function(){
     function toggleChangeBtn() {
         var slideIndex = $('.slide').index($('.active'));
